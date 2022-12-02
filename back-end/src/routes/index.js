542eaa8e49f0285bcf9e863b6dad12515e76158d
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2022-12-02 16:11:25 +0530

    Added a login route to the server

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,7 +1,9 @@
+import { loginRoute } from './loginRoute';
 import { signUpRoute } from './SignUpRoute';
 import { testRoute } from './testRoute';
 
 export const routes = [
+    loginRoute,
     signUpRoute,
     testRoute,
 ];
