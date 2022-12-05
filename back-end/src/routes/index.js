23c2a22e6e9fe91d32427207918bd8ca9f56fe86
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2022-12-05 16:11:08 +0530

    Add JWTs to the user info page

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,9 +1,11 @@
 import { loginRoute } from './loginRoute';
 import { signUpRoute } from './SignUpRoute';
 import { testRoute } from './testRoute';
+import { updateUserInfoRoute } from './updateUserInfoRoute';
 
 export const routes = [
     loginRoute,
     signUpRoute,
     testRoute,
+    updateUserInfoRoute,
 ];
--- back-end/src/routes/updateUserInfoRoute.js
+++ back-end/src/routes/updateUserInfoRoute.js
@@ -1,51 +1,51 @@
-import { Jwt } from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 import { ObjectID } from 'mongodb';
-import { getDbConnection } from "../db";
+import { getDbConnection } from '../db';
 
-export const updateUserInfoRoute =  {
+export const updateUserInfoRoute = {
     path: '/api/users/:userId',
     method: 'put',
     handler: async (req, res) => {
         const { authorization } = req.headers;
         const { userId } = req.params;
 
-        const updates = ({
+        const updates = (({
             favoriteFood,
             hairColor,
             bio,
-        }) =>  ({
+        }) => ({
             favoriteFood,
             hairColor,
             bio,
-        })(req.body);
+        }))(req.body);
 
         if (!authorization) {
-            return res.status(401).json({message: 'No authorization header sent'});
+            return res.status(401).json({ message: 'No authorization header sent' });
         }
-        //Bearer lkj;jkkl.kfjlak.lkjlfkaj
+
         const token = authorization.split(' ')[1];
-        Jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-            if (err) return res.status(401).json({ message: 'Unable to verify token'});
+
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+            if (err) return res.status(401).json({ message: 'Unable to verify token' });
 
             const { id } = decoded;
 
-            if (id !== userId) res.status(403).json({message: 'Not allowed to update user\'s data'});
+            if (id !== userId) return res.status(403).json({ message: 'Not allowed to update that user\'s data' });
 
             const db = getDbConnection('react-auth-db');
             const result = await db.collection('users').findOneAndUpdate(
-                {_id: ObjectID(id)},
-                {$set: {info: updates}},
-                {returnOriginal: false},
+                { _id: ObjectID(id) },
+                { $set: { info: updates } },
+                { returnOriginal: false },
             );
-                const { email, isVerified, info } = result.value;
+            const { email, isVerified, info } = result.value;
 
-                jwt.sign( { id, email, isVerified,info }, process.env.JWT_SECRET,{ expiredIn: '[2d'}, (err, token) => {
-                    if (err) {
-                        return res.status(200).json(err);
-                    }
-                    res.status(200).json({ token });
-                } );
+            jwt.sign({ id, email, isVerified, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
+                if (err) {
+                    return res.status(200).json(err);
+                }
+                res.status(200).json({ token });
+            });
         })
-
     }
 }
