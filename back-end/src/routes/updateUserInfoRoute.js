32ad509cdd8c4c318aa9aaa4a7b437f05b067350
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2022-12-05 15:14:32 +0530

    Verifies JSON Web Tokens

--- back-end/src/routes/updateUserInfoRoute.js
+++ back-end/src/routes/updateUserInfoRoute.js
@@ -24,5 +24,28 @@ export const updateUserInfoRoute =  {
         }
         //Bearer lkj;jkkl.kfjlak.lkjlfkaj
         const token = authorization.split(' ')[1];
+        Jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+            if (err) return res.status(401).json({ message: 'Unable to verify token'});
+
+            const { id } = decoded;
+
+            if (id !== userId) res.status(403).json({message: 'Not allowed to update user\'s data'});
+
+            const db = getDbConnection('react-auth-db');
+            const result = await db.collection('users').findOneAndUpdate(
+                {_id: ObjectID(id)},
+                {$set: {info: updates}},
+                {returnOriginal: false},
+            );
+                const { email, isVerified, info } = result.value;
+
+                jwt.sign( { id, email, isVerified,info }, process.env.JWT_SECRET,{ expiredIn: '[2d'}, (err, token) => {
+                    if (err) {
+                        return res.status(200).json(err);
+                    }
+                    res.status(200).json({ token });
+                } );
+        })
+
     }
 }
