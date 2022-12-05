6212cfed8692ac5b6a307a22958a71f7b8e59246
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2022-12-05 12:43:38 +0530

    Add JWTs to login page

--- front-end/src/pages/LogInPage.js
+++ front-end/src/pages/LogInPage.js
@@ -1,15 +1,27 @@
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import { useToken } from '../auth/useToken';
 
 export const LogInPage = () => {
+    const [token, setToken] = useToken();
+
     const [errorMessage, setErrorMessage] = useState('');
+
     const [emailValue, setEmailValue] = useState('');
     const [passwordValue, setPasswordValue] = useState('');
 
     const history = useHistory();
 
     const onLogInClicked = async () => {
-        alert('Log in not implemented yet');
+        const response = await axios.post('/api/login', {
+            email: emailValue,
+            password: passwordValue,
+        });
+
+        const { token } = response.data;
+        setToken(token);
+        history.push('/');
     }
 
     return (
